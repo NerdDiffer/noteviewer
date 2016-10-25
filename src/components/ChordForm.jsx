5b1edece3e1a30782f82d79ec338ca2730e1752f
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-25 15:22:46 -0700

    refactor(ChordForm): Change to functional stateless component
    Does not need knowledge of state

--- src/components/ChordForm.jsx
+++ src/components/ChordForm.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ChordForm extends Component {
-  constructor(props) {
-    super(props);
-  }
+const ChordForm = (props) => {
+  const { handleTonicChange, handleTypeChange } = props;
 
-  render() {
-    const { handleTonicChange, handleTypeChange } = this.props;
-
-    return (
-      <div className="controls">
-        <label htmlFor="chord_tonic">Tonic</label>
-        <input
-          type="text"
-          placeholder="Change tonic"
-          onChange={handleTonicChange}
-          id="chord_tonic"
-        />
-        <br />
-        <label htmlFor="chord_type">Type</label>
-        <input
-          type="text"
-          placeholder="Change type"
-          onChange={handleTypeChange}
-          id="chord_type"
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="controls">
+      <label htmlFor="chord_tonic">Tonic</label>
+      <input
+        type="text"
+        placeholder="Change tonic"
+        onChange={handleTonicChange}
+        id="chord_tonic"
+      />
+      <br />
+      <label htmlFor="chord_type">Type</label>
+      <input
+        type="text"
+        placeholder="Change type"
+        onChange={handleTypeChange}
+        id="chord_type"
+      />
+    </div>
+  );
 }
 
 export default ChordForm;
