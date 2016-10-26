82d40da88c08238b7f6e28a8fb0386ef043e2d58
Rafael Espinoza <nerddiffer@gmail.com>
2016-10-26 14:53:43 -0700

    build(styles): Configure webpack
    With unminified assets from semantic-ui library build is over 9MB,
    which is fine for local development purposes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PATHS = {
   SRC: path.join(__dirname, 'src'),
   PUBLIC: path.join(__dirname, 'public'),
-  BUILD: path.join(__dirname, 'public/build')
+  BUILD: path.join(__dirname, 'public/build'),
+  SEMANTIC_UI: path.join(__dirname, 'semantic/dist')
 };
 
 module.exports = {
   devtool: 'inline-source-map',
   entry: [
-    `${PATHS.SRC}/index.jsx`
+    `${PATHS.SEMANTIC_UI}/semantic.js`,
+    `${PATHS.SEMANTIC_UI}/semantic.css`,
+    `${PATHS.SRC}/index.jsx`,
   ],
   output: {
     path: PATHS.BUILD,
@@ -21,6 +26,13 @@ module.exports = {
     modulesDirectories: ['node_modules'],
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ],
   module: {
     loaders: [
       {
@@ -36,6 +48,16 @@ module.exports = {
         include: /node_modules\/tonal-.*/,
         test: /\.json$/
       },
+      {
+        loader: ExtractTextPlugin.extract('css?inlineSourceMap'),
+        include: PATHS.SEMANTIC_UI,
+        test: /\.css$/,
+      },
+      {
+        loader: 'file',
+        include: PATHS.SEMANTIC_UI,
+        test: /\.(eot|png|woff|woff2|svg|ttf)$/,
+      }
     ]
   },
   devServer: {
